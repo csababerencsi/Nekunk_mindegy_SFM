b011cc1f7fc89cd55324bd7bad1e2ca33520b102
ancsankristof1119 <ancsankristof@gmail.com>
2021-03-29 17:24:38 +0200

    03_29kristof1

--- src/main/java/hu/unideb/App.java
+++ src/main/java/hu/unideb/App.java
@@ -1,33 +1,28 @@
 package hu.unideb;
-
+import hu.unideb.controller.fxcontroller;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-/**
- * JavaFX App
- */
+import java.io.IOException;
+
+
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-
-        Button gomb1 = new Button("Nyomd meg");
-        var scene = new Scene(gomb1, 200, 100);
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("/fxml/fxmlfajl.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Kave Gep");
         stage.setScene(scene);
         stage.show();
     }
 
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
 
--- src/main/java/hu/unideb/controller/fxcontroller.java
+++ src/main/java/hu/unideb/controller/fxcontroller.java
@@ -1,23 +1,66 @@
 package hu.unideb.controller;
-
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class fxcontroller {
-
+    @FXML
+    private Button espresso;
 
     @FXML
-    private Label label;
+    private Button cappucino;
 
-    public fxcontroller(Label label) {
-        this.label = label;
-    }
+    @FXML
+    private Button hchocolate;
 
-    public void handleButtonAction(ActionEvent actionEvent) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    @FXML
+    private Button coffewmilk;
+
+    @FXML
+    private TextField valueMoney;
+
+    @FXML
+    private Label currentMoney;
+
+    @FXML
+    private TextField valueCoffe;
+
+    @FXML
+    private Label currentCoffe;
+
+    @FXML
+    private TextField valueWater;
+
+    @FXML
+    private Label currentWater;
+
+    @FXML
+    private TextField valueMilk;
+
+    @FXML
+    private Label currentMilk;
+
+    @FXML
+    private TextField valueSugar;
+
+    @FXML
+    private Label currentSugar;
+
+    @FXML
+    private Button saveMoney;
+
+    @FXML
+    private Button saveCoffe;
+
+    @FXML
+    private Button saveWater;
+
+    @FXML
+    private Button saveMilk;
+
+    @FXML
+    private Button saveSugar;
+
 }
